15c115be4536c7bb0fe8e1042ee9c6c7870ef9e8
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-02-19 17:17:56 +0100

    Second test

--- restructure_html.py
+++ restructure_html.py
@@ -1,8 +1,8 @@
 import sys
 import re
 
-input_file = sys.argv[1]
-output_file = "output.txt"
+input_file = sys.argv[1] + ".html"
+output_file = sys.argv[1] + ".txt"
 
 with open(input_file, 'r', encoding='utf-8') as file:
     html = file.read()
@@ -15,7 +15,16 @@ if not body_match:
     sys.exit()
 
 body_content = body_match.group(1)
-elements = re.findall(r'<[^>]+>|[^<]+', body_content)
+elements = re.findall(r'<[^>]+>|&[^;]+;|[^<&]+', body_content)
+
+classified_elements = []
+for item in elements:
+    if item.startswith('<') and item.endswith('>'):
+        classified_elements.append({'type': '0', 'content': item})
+    elif item.startswith('&') and item.endswith(';'):
+        classified_elements.append({'type': '1', 'content': item})
+    else:
+        classified_elements.append({'type': '2', 'content': item})
 
 with open(output_file, 'w', encoding='utf-8') as outfile:
-    outfile.write(str(elements))
+    outfile.write(str(classified_elements))
